8d0ba240f2a6a151bfb54be5edfa2ee42f049a28
IrinaMar12 <im1351236@gmail.com>
2024-12-19 18:28:18 +0300

    "Условная конструкция. Операторы if, elif, else"

---
+++ dicts.py
@@ -0,0 +1,31 @@
+         # 1
+#-данные по условию задачи: три целых числа
+
+first=int(input('введите  первое число: '))
+second=int(input('введите второе число: '))
+third=int(input('введите  третье число: '))
+
+if first == second and  first == third :
+    print(3)
+elif second == first or first == third or second == third:
+    print(2)
+else:
+    print(0)     #- равных между собой чисел нет.
+
+       #2
+
+first=int(input('введите  первое число: '))
+second=int(input('введите второе число: '))
+third=int(input('введите  третье число: '))
+
+if first == second and  first == third :
+    print(3)
+elif second == first or first == third or second == third:
+    print(2)
+else:
+ print(0)
+
+
+
+
+
